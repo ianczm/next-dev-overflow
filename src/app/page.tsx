610366cf4187b3460fabc78f05dfb034297170bf
Ian Chong <ianczm@live.co.uk>
2024-11-03 19:55:41 +0800

    Feature: Add site logo and navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/navigation/navbar";
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
     </>
   );
